39ece2805165aa72caa0927b4c8c7ba742164bf6
ANALICE-CTDS20 <silvaa2465@gmail.com>
2025-10-17 16:24:18 -0300

    Add files via upload

---
+++ 07/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// Solicita os números inicial e final ao usuário
+using System.ComponentModel;
+
+Console.Write("Digite o número inicial da tabuada: ");
+int start = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o número final da tabuada: ");
+int end = int.Parse(Console.ReadLine());
+
+Console.WriteLine(); // Espaço
+
+// Loop externo para o intervalo
+for (int num = start; num <= end; num++)
+{
+    Console.WriteLine($"Tabuada do {num}:");
+    Console.WriteLine(new string('-', 20));
+
+    // Loop interno de 1 a 10
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($"{num} x {i} = {num * i}");
+    }
+
+    Console.WriteLine(); // Espaço entre tabuadas
+}
+
+Console.WriteLine("Tabuadas concluídas. Pressione qualquer tecla para sair...");
+Console.ReadKey();
+        
+
+
+
+
